31647c23cc5cc58c3fcef10e4345a5e720e7fe4e
Lou Böcker <lou@boecker.dev>
2026-07-02 13:54:32 +0200

    add sounds to item collect and show how many items a team has collected

--- src/main/kotlin/dev/boecker/cherrycave/bingo/game/state/ingame/CheckWin.kt
+++ src/main/kotlin/dev/boecker/cherrycave/bingo/game/state/ingame/CheckWin.kt
@@ -3,6 +3,8 @@ package dev.boecker.cherrycave.bingo.game.state.ingame
 import dev.boecker.cherrycave.bingo.game.BingoGameManager
 import dev.boecker.cherrycave.bingo.game.team.BingoTeams
 import dev.boecker.cherrycave.bingo.game.team.getBingoTeam
+import net.kyori.adventure.key.Key
+import net.kyori.adventure.sound.Sound
 import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.Material
 import org.bukkit.entity.Player
@@ -13,11 +15,23 @@ fun checkIfBingoItem(material: Material, player: Player, gameManager: BingoGameM
 
     val team = player.getBingoTeam(gameManager) ?: return
 
+    val successCollectSound = Sound.sound(Key.key("block.note_block.bell"), Sound.Source.PLAYER, 1f, 1f)
+    val othersCollectSound = Sound.sound(Key.key("block.note_block.bass"), Sound.Source.PLAYER, 1f, 1f)
+
     if (gameManager.bingoBoard!!.contains(material) && !gameManager.ingameState.collectedItems[team]!!.contains(material)) {
         gameManager.plugin.server.broadcast(
             MiniMessage.miniMessage()
-                .deserialize("<${team.teamColor}>${team.teamName} <white>collected</white> <blue><lang:${material.translationKey()}><white>!")
+                .deserialize("<${team.teamColor}>${team.teamName} <white>collected</white> <blue><lang:${material.translationKey()}><white>! <gray>(${gameManager.ingameState.collectedItems[team]!!.size}/${gameManager.bingoBoard!!.size}")
         )
+        val teamUUIDs = gameManager.teams[team]!!
+        gameManager.plugin.server.onlinePlayers.forEach { player ->
+            if (player.uniqueId in teamUUIDs) {
+                player.playSound(successCollectSound, Sound.Emitter.self())
+            } else {
+                player.playSound(othersCollectSound, Sound.Emitter.self())
+            }
+        }
+
         gameManager.ingameState.collectedItems[team]!!.add(material)
 
         if (checkForBingo(team, gameManager)) {
@@ -42,19 +56,14 @@ fun checkForBingo(team: BingoTeams, gameManager: BingoGameManager): Boolean {
         if (twoDimensionalBoard.all { row -> row[i] in gameManager.ingameState.collectedItems[team]!! }) return true
     }
 
-
     val fromLeftIterator = (0 until boardSize)
-    println("from left iterator: $fromLeftIterator")
     if (fromLeftIterator.all { i ->
             twoDimensionalBoard[i][i] in gameManager.ingameState.collectedItems[team]!!
         }) return true
 
 
     val fromRightIterator = ((boardSize - 1) downTo 0)
-    println("from right iterator: $fromRightIterator")
-    if (fromRightIterator.all { i ->
-            twoDimensionalBoard[i][i] in gameManager.ingameState.collectedItems[team]!!
-        }) return true
-
-    return false
+    return fromRightIterator.all { i ->
+        twoDimensionalBoard[i][i] in gameManager.ingameState.collectedItems[team]!!
+    }
 }
